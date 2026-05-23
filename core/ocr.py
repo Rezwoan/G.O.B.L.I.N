921b8f090d2be8ce4d5a94ee77caeff7067ab0eb
Rezwoan <frezwoan@gmail.com>
2026-05-24 01:15:15 +0600

    new vision new start

--- core/ocr.py
+++
@@ -1,9 +0,0 @@
-class OCRReader:
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def read_region(self, frame, region_id) -> int | None:
-        return None
-
-    def read_raw(self, frame, bbox_rel) -> int | None:
-        return None
